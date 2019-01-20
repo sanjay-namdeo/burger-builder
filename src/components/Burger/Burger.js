75c439c5cb03b9b52246825229e6ac0409f0a418
Sanjay Namdeo <sanjay.namdeo@live.com>
2019-01-20 10:27:51 +0100

    Dynamically pass ingredients and build burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const Burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients).map(element => {
+    return [...Array(props.ingredients[element])].map((_, index) => {
+      return <BurgerIngredient key={element + index} type={element} />;
+    });
+  });
+
+  console.log(transformedIngredients);
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      meat: 1,
+      cheese: 2,
+      bacon: 1,
+      salad: 2
+    }
+  };
+
   render() {
     return (
       <>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </>
     );
